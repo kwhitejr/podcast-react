da1dab7a9e1682bf2b84515bfb3efe03a18210db
kwhitejr <kwhitejr.edu@gmail.com>
2016-05-15 19:17:43 -1000

    added second component; still watching

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import Header from './header.jsx';
+
 class App extends React.Component {
   render () {
-    return <p> Hello Boss!</p>;
+    return (
+      <div>
+        <p> Hello Boss!</p>
+        <Header />
+      </div>
+    );
   }
 }
 
