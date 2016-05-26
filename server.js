5ef7ffe1350145c9974fbcdc84637796db81daa5
kwhitejr <kwhitejr.edu@gmail.com>
2016-05-26 09:40:04 -1000

    installed basic mongodb packages

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var episodeSchema = mongoose.Schema({
+  name: String,
+  author: String
+});
+// collection name will get pluralized by mongoose
+var Episode = mongoose.model('Episode', episodeSchema);
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  var server = app.listen(CONFIG.PORT, function() {
+    console.log('Listening to port', CONFIG.PORT);
+  });
+});
