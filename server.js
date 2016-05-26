ec9172171e60757354f8306eacbd9ce5147629fb
kwhitejr <kwhitejr.edu@gmail.com>
2016-05-26 13:03:27 -1000

    server listening

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express'),
+    path = require('path'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
 
 var episodeSchema = mongoose.Schema({
   name: String,
@@ -11,12 +14,18 @@ var Episode = mongoose.model('Episode', episodeSchema);
 
 var app = express();
 
+app.use(express.static(path.resolve(__dirname, './src/client')));
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.route('/')
+  .get(function (req, res) {
+    res.send('index');
+  });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  var server = app.listen(CONFIG.PORT, function() {
-    console.log('Listening to port', CONFIG.PORT);
+  var server = app.listen(3000, function() {
+    console.log('Listening to port', 3000);
   });
 });
