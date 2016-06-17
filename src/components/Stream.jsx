1e229a65c08f189d6a8eca14e1be1b5a0a265d86
kwhitejr <kwhitejr.edu@gmail.com>
2016-06-16 18:21:02 -1000

    added stock soundcloud callback html

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { PlayButton, Progress, Timer, Icons } from 'react-soundplayer/components';
+import { SoundPlayerContainer } from 'react-soundplayer/addons';
 
-function Stream({ tracks = [] }) {
-  return (
-    <div>
-      {
-        tracks.map((track, key) => {
-          return <div className="track" key={key}>{track.title}</div>;
-        })
-      }
+const { SoundCloudLogoSVG } = Icons;
+
+const CLIENT_ID = '89289477605a115f4826be4c766dcdb3';
+// const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
+const REDIRECT_URI = 'https://soundcloud.com/thegrindwithkevin';
+
+export default React.createClass({
+
+  render: function () {
+    return <div>
+      <SoundPlayerContainer
+        clientId={CLIENT_ID}
+        resolveUrl={REDIRECT_URI}
+      >
+        <PlayButton />
+        <Progress />
+        <Timer />
+      </SoundPlayerContainer>
     </div>
-  );
-}
-
-export default Stream;
+  }
+});
