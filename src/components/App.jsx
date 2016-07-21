9c528487443327e184800a80167d74196120ec24
kwhitejr <kwhitejr.edu@gmail.com>
2016-07-20 21:02:46 -1000

    add button ease in

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,6 +21,7 @@ export const App = React.createClass({
 
   componentDidUpdate: function () {
     this.smoothScroll();
+    this.scrollToTop();
   },
 
   loadEpisodes: function () {
@@ -50,6 +51,24 @@ export const App = React.createClass({
           return false;
         }
       }
+    });
+  },
+
+  scrollToTop: function () {
+    var offset = 250;
+    var duration = 300;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(duration);
+      } else {
+        $('.back-to-top').fadeOut(duration);
+      }
+    });
+
+    $('.back-to-top').click(function(event) {
+      event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, duration);
+      return false;
     });
   },
 
